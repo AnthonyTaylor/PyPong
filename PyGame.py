974fe32c13822d773ef1c30a3349dfc30cf30e4f
AnthonyTaylor <AnTaylor@Northamptonshire.gov.uk>
2020-06-10 21:57:21 +0100

    Adding init position for the paddle

--- PyGame.py
+++ PyGame.py
@@ -1,11 +1,14 @@
 import pygame
 
-
 # Vars
 WIDTH = 1200
 HEIGHT = 600
 BORDER = 20
-fg_colour = pygame.Color("white")
+fg_colour = pygame.Color("yellow")
+paddle_height = 50
+paddle_width = 10
+paddle_padding = 20
+
 
 # Draw scenario
 pygame.init()
@@ -16,6 +19,12 @@ screen = pygame.display.set_mode((WIDTH,HEIGHT)) # set window size
 pygame.draw.rect(screen, fg_colour, pygame.Rect((0,0),(WIDTH,BORDER)))
 pygame.draw.rect(screen, fg_colour, pygame.Rect((0,HEIGHT - BORDER),(WIDTH,HEIGHT)))
 pygame.draw.rect(screen, fg_colour, pygame.Rect((0,0),(BORDER,HEIGHT)))
+
+# position paddle
+pygame.draw.rect(screen, fg_colour, pygame.Rect(
+    (WIDTH - paddle_width - paddle_padding, (HEIGHT / 2) - (paddle_height / 2)),
+    (paddle_width - paddle_padding, paddle_height)))
+
 
 # draw to screen
 pygame.display.flip()
